5a68de068f416e6f6568f7642656c564915296ce
EsneiderCdD <ecd.97@hotmail.com>
2026-01-25 12:10:06 -0500

    starting project

---
+++ src/reusable/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+
+
+function Navbar() {
+
+  return (
+    <>
+    <section>
+        <nav>
+            <ul>
+                <li><a href="#">Tema Uno</a></li>
+                <li><a href="#">Tema Dos</a></li>
+                <li><a href="#">Tema Tres</a></li>
+            </ul>
+        </nav>
+    </section>
+    </>
+  )
+}
+
+export default Navbar
